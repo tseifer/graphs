4899dd21457ec309fd3277e78e06143c1d5a610c
Tidhar <tseifer@yahoo.com>
2020-09-25 09:03:09 +0300

    added chart.js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
+import Chart from 'chart.js';
+
+import {GRAPH_DATA} from './graph-data';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,54 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'canvas';
+
+  @ViewChild('mycanvas', { static: true })
+  canvas: ElementRef<HTMLCanvasElement>;
+
+  private ctx: CanvasRenderingContext2D;
+
+  ngOnInit(): void {
+    this.ctx = this.canvas.nativeElement.getContext('2d');
+    this.draw();
+  }
+
+  draw() {
+    var myChart = new Chart(this.ctx, {
+      type: 'line',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    });
+  }
+
 }
---
+++ src/app/graph-data.ts
@@ -0,0 +1,242 @@
+export const GRAPH_DATA = {
+    'chart': {
+        'caption'           : 'CPURate on CICD-VIEW [Jobname_CICSBELL]',
+        'subcaption'        : 'Between 2020-08-13 08:00:00 and 2020-09-25 08:59:00',
+        'xAxisName'         : 'Time',
+        'yAxisName'         : 'Average Sum',
+        'YAxisMaxValue'     : '52.350279345512',
+        'YAxisMinValue'     : '5.4177198266983',
+        'showLimits'        : '1',
+        'chartRightMargin'  : '65',
+        'showvalues'        : '0',
+        'anchorRadius'      : '2',
+        'showNames'         : '1',
+        'showDivLineValue'  : '1',
+        'anchorBgAlpha'     : '0',
+        'connectNullData'   : '1',
+        'numDivLines'       : '8',
+        'adjustDiv'         : '0',
+        'yAxisValueDecimals': '3',
+        'decimals'          : '3',
+        'palette'           : '3',
+        'categories'        : {
+            'category': [{'label': '2020-08-13 00:26:19', 'showName': '0'}, {
+                'label'   : '2020-08-13 17:55:14',
+                'showName': '1'
+            }, {'label': '2020-08-14 11:24:09', 'showName': '0'}, {
+                'label'   : '2020-08-15 04:53:04',
+                'showName': '0'
+            }, {'label': '2020-08-15 22:21:59', 'showName': '0'}, {
+                'label'   : '2020-08-16 15:50:54',
+                'showName': '0'
+            }, {'label': '2020-08-17 09:19:49', 'showName': '0'}, {
+                'label'   : '2020-08-18 02:48:44',
+                'showName': '0'
+            }, {'label': '2020-08-18 20:17:40', 'showName': '0'}, {
+                'label'   : '2020-08-19 13:46:35',
+                'showName': '0'
+            }, {'label': '2020-08-20 07:15:30', 'showName': '0'}, {
+                'label'   : '2020-08-21 00:44:25',
+                'showName': '0'
+            }, {'label': '2020-08-21 18:13:20', 'showName': '0'}, {
+                'label'   : '2020-08-22 11:42:15',
+                'showName': '1'
+            }, {'label': '2020-08-23 05:11:10', 'showName': '0'}, {
+                'label'   : '2020-08-23 22:40:05',
+                'showName': '0'
+            }, {'label': '2020-08-24 16:09:00', 'showName': '0'}, {
+                'label'   : '2020-08-25 09:37:55',
+                'showName': '0'
+            }, {'label': '2020-08-26 03:06:50', 'showName': '0'}, {
+                'label'   : '2020-08-26 20:35:45',
+                'showName': '0'
+            }, {'label': '2020-08-27 14:04:40', 'showName': '0'}, {
+                'label'   : '2020-08-28 07:33:35',
+                'showName': '0'
+            }, {'label': '2020-08-29 01:02:30', 'showName': '0'}, {
+                'label'   : '2020-08-29 18:31:26',
+                'showName': '0'
+            }, {'label': '2020-08-30 12:00:21', 'showName': '0'}, {
+                'label'   : '2020-08-31 05:29:16',
+                'showName': '1'
+            }, {'label': '2020-08-31 22:58:11', 'showName': '0'}, {
+                'label'   : '2020-09-01 16:27:06',
+                'showName': '0'
+            }, {'label': '2020-09-02 09:56:01', 'showName': '0'}, {
+                'label'   : '2020-09-03 03:24:56',
+                'showName': '0'
+            }, {'label': '2020-09-03 20:53:51', 'showName': '0'}, {
+                'label'   : '2020-09-04 14:22:46',
+                'showName': '0'
+            }, {'label': '2020-09-05 07:51:41', 'showName': '0'}, {
+                'label'   : '2020-09-06 01:20:36',
+                'showName': '0'
+            }, {'label': '2020-09-06 18:49:31', 'showName': '0'}, {
+                'label'   : '2020-09-07 12:18:26',
+                'showName': '0'
+            }, {'label': '2020-09-08 05:47:21', 'showName': '0'}, {
+                'label'   : '2020-09-08 23:16:16',
+                'showName': '1'
+            }, {'label': '2020-09-09 16:45:12', 'showName': '0'}, {
+                'label'   : '2020-09-10 10:14:07',
+                'showName': '0'
+            }, {'label': '2020-09-11 03:43:02', 'showName': '0'}, {
+                'label'   : '2020-09-11 21:11:57',
+                'showName': '0'
+            }, {'label': '2020-09-12 14:40:52', 'showName': '0'}, {
+                'label'   : '2020-09-13 08:09:47',
+                'showName': '0'
+            }, {'label': '2020-09-14 01:38:42', 'showName': '0'}, {
+                'label'   : '2020-09-14 19:07:37',
+                'showName': '0'
+            }, {'label': '2020-09-15 12:36:32', 'showName': '0'}, {
+                'label'   : '2020-09-16 06:05:27',
+                'showName': '0'
+            }, {'label': '2020-09-16 23:34:22', 'showName': '0'}, {
+                'label'   : '2020-09-17 17:03:17',
+                'showName': '1'
+            }, {'label': '2020-09-18 10:32:12', 'showName': '0'}, {
+                'label'   : '2020-09-19 04:01:07',
+                'showName': '0'
+            }, {'label': '2020-09-19 21:30:02', 'showName': '0'}, {
+                'label'   : '2020-09-20 14:58:58',
+                'showName': '0'
+            }, {'label': '2020-09-21 08:27:53', 'showName': '0'}, {
+                'label'   : '2020-09-22 01:56:48',
+                'showName': '0'
+            }, {'label': '2020-09-22 19:25:43', 'showName': '0'}, {
+                'label'   : '2020-09-23 12:54:38',
+                'showName': '0'
+            }, {'label': '2020-09-24 06:23:33', 'showName': '0'}, {'label': '2020-09-24 23:52:28', 'showName': '0'}]
+        },
+        'dataset'           : [{
+            'seriesName'         : 'CPURate AVERAGE=29.2366 STDDEV=9.6701',
+            'renderAs'           : 'Line',
+            'parentYAxis'        : 'P',
+            'color'              : 'AFD8F8',
+            'alpha'              : '100',
+            'anchorAlpha'        : '100',
+            'lineThickness'      : '2',
+            'anchorBorderColor'  : 'AFD8F8',
+            'anchorRadius'       : '3',
+            'anchorBgColor'      : 'ffffff',
+            'anchorBgAlpha'      : '100',
+            'topErrorBar'        : '1',
+            'bottomErrorBar'     : '1',
+            'errorbarwidth'      : '25',
+            'errorbarthickness'  : '1',
+            'errorbarcolor'      : 'AFD8F8',
+            'bottomErrorBarWidth': '10',
+            'topErrorBarWidth'   : '10',
+            'set'                : [{
+                'value'           : '31.0182',
+                'toolText'        : '<b>CPURate<\/b> <u>Value<\/u>: 31.0182  <u>Start<\/u>: 2020-08-13 00:26:19 <u>End<\/u>: 2020-08-13 17:55:14 <u>Samples<\/u>: 449 <u>Min local spread<\/u>: 10.626 <u>Max local spread<\/u>: 51.022 <u>Std Dev<\/u>: 9.8737  <p align=\'center\'>[Click for drill down<\/p>]',
+                'link'            : 'javascript:zoomin(1597267560.4661,1597330560.5339, [8], [44], 138, \'oneline\', 0, 0, null, null)',
+                'bottomErrorValue': '20.3922',
+                'topErrorValue'   : '20.0038'
+            }, {
+                'value'           : '31.7662',
+                'toolText'        : '<b>CPURate<\/b> <u>Value<\/u>: 31.7662  <u>Start<\/u>: 2020-08-13 17:55:14 <u>End<\/u>: 2020-08-14 11:24:09 <u>Samples<\/u>: 404 <u>Min local spread<\/u>: 9.351 <u>Max local spread<\/u>: 50.059 <u>Std Dev<\/u>: 9.8668  <p align=\'center\'>[Click for drill down<\/p>]',
+                'link'            : 'javascript:zoomin(1597330500.5339,1597393500.6017, [8], [44], 138, \'oneline\', 0, 0, null, null)',
+                'bottomErrorValue': '22.4152',
+                'topErrorValue'   : '18.2928'
+            }, {
+                'value'           : '22.8376',
+                'toolText'        : '<b>CPURate<\/b> <u>Value<\/u>: 22.8376  <u>Start<\/u>: 2020-08-14 11:24:09 <u>End<\/u>: 2020-08-15 04:53:04 <u>Samples<\/u>: 145 <u>Min local spread<\/u>: 6.746 <u>Max local spread<\/u>: 39.087 <u>Std Dev<\/u>: 6.5285  <p align=\'center\'>[Click for drill down<\/p>]',
+                'link'            : 'javascript:zoomin(1597393440.6017,1597456440.6695, [8], [44], 138, \'oneline\', 0, 0, null, null)',
+                'dashed'          : '1',
+                'bottomErrorValue': '16.0916',
+                'topErrorValue'   : '16.2494'
+            }, [], {'dashed': '1'}, {'dashed': '1'}, {'dashed': '1'}, {'dashed': '1'}, {'dashed': '1'}, {'dashed': '1'}, {'dashed': '1'}, {'dashed': '1'}, {'dashed': '1'}, {'dashed': '1'}, {'dashed': '1'}, {'dashed': '1'}, {'dashed': '1'}, {'dashed': '1'}, {'dashed': '1'}, {'dashed': '1'}, {'dashed': '1'}, {'dashed': '1'}, {'dashed': '1'}, {'dashed': '1'}, {'dashed': '1'}, {'dashed': '1'}, {'dashed': '1'}, {
+                'value'           : '34.9948',
+                'toolText'        : '<b>CPURate<\/b> <u>Value<\/u>: 34.9948  <u>Start<\/u>: 2020-09-01 16:27:06 <u>End<\/u>: 2020-09-02 09:56:01 <u>Samples<\/u>: 17 <u>Min local spread<\/u>: 27.326 <u>Max local spread<\/u>: 46.02 <u>Std Dev<\/u>: 6.146  <p align=\'center\'>[Click for drill down<\/p>]',
+                'link'            : 'javascript:zoomin(1598966820.2966,1599029820.3644, [8], [44], 138, \'oneline\', 0, 0, null, null)',
+                'bottomErrorValue': '7.6688',
+                'topErrorValue'   : '11.0252'
+            }, [], {
+                'value'           : '25.0775',
+                'toolText'        : '<b>CPURate<\/b> <u>Value<\/u>: 25.0775  <u>Start<\/u>: 2020-09-03 03:24:56 <u>End<\/u>: 2020-09-03 20:53:51 <u>Samples<\/u>: 197 <u>Min local spread<\/u>: 9.521 <u>Max local spread<\/u>: 44.284 <u>Std Dev<\/u>: 7.5613  <p align=\'center\'>[Click for drill down<\/p>]',
+                'link'            : 'javascript:zoomin(1599092640.4322,1599155640.5, [8], [44], 138, \'oneline\', 0, 0, null, null)',
+                'dashed'          : '1',
+                'bottomErrorValue': '15.5565',
+                'topErrorValue'   : '19.2065'
+            }, [], {'dashed': '1'}, {'dashed': '1'}, {'dashed': '1'}, {'dashed': '1'}, {'dashed': '1'}, {'dashed': '1'}, {'dashed': '1'}, {'dashed': '1'}, {'dashed': '1'}, {'dashed': '1'}, {'dashed': '1'}, {'dashed': '1'}, {'dashed': '1'}, {'dashed': '1'}, {'dashed': '1'}, {'dashed': '1'}, {'dashed': '1'}, {'dashed': '1'}, {'dashed': '1'}, {'dashed': '1'}, {'dashed': '1'}, {'dashed': '1'}, {'dashed': '1'}, {'dashed': '1'}, {'dashed': '1'}, {'dashed': '1'}, {'dashed': '1'}, {'dashed': '1'}, {
+                'value'           : '24.5728',
+                'toolText'        : '<b>CPURate<\/b> <u>Value<\/u>: 24.5728  <u>Start<\/u>: 2020-09-24 23:52:28 <u>End<\/u>: 2020-09-25 17:21:23 <u>Samples<\/u>: 37 <u>Min local spread<\/u>: 13.819 <u>Max local spread<\/u>: 33.718 <u>Std Dev<\/u>: 5.5823  <p align=\'center\'>[Click for drill down<\/p>]',
+                'link'            : 'javascript:zoomin(1600980720.4661,1601043720.5339, [8], [44], 138, \'oneline\', 0, 0, null, null)',
+                'bottomErrorValue': '10.7538',
+                'topErrorValue'   : '9.1452'
+            }]
+        }, {
+            'seriesName'       : 'Historic Thresholds',
+            'renderAs'         : 'Area',
+            'parentYAxis'      : 'P',
+            'color'            : '2db300',
+            'showPlotBorder'   : '0',
+            'alpha'            : '80',
+            'drawAnchors'      : '0',
+            'plotGradientColor': '\'\'',
+            'set'              : [{
+                'value'   : '51.021999359131',
+                'lValue'  : '10.236000061035',
+                'toolText': ''
+            }, {'value': '50.05899810791', 'lValue': '9.3509998321533', 'toolText': ''}, {
+                'value'   : '39.0870018005371',
+                'lValue'  : '6.74599981307983',
+                'toolText': ''
+            }, [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], {
+                'value'   : '46.555199432373',
+                'lValue'  : '21.549799728394',
+                'toolText': ''
+            }, [], {
+                'value'   : '44.2840003967285',
+                'lValue'  : '9.52099990844727',
+                'toolText': ''
+            }, [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], {
+                'value'   : '33.6520004272461',
+                'lValue'  : '13.8190002441406',
+                'toolText': ''
+            }]
+        }, {
+            'seriesName'   : 'Historic Average',
+            'renderAs'     : 'Line',
+            'parentYAxis'  : 'P',
+            'color'        : 'FAFAD2',
+            'alpha'        : '50',
+            'lineThickness': '2',
+            'dashed'       : '1',
+            'lineDashLen'  : '5',
+            'lineDashGap'  : '5',
+            'drawAnchors'  : '0',
+            'set'          : [{'value': '30.930085208527', 'toolText': ''}, {
+                'value'   : '31.894113579127',
+                'toolText': ''
+            }, {
+                'value'   : '22.469630491323',
+                'toolText': ''
+            }, [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], {
+                'value'   : '33.896590614319',
+                'toolText': ''
+            }, [], {
+                'value'   : '26.057961609158',
+                'toolText': ''
+            }, [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], {
+                'value'   : '23.271200180054',
+                'toolText': ''
+            }]
+        }],
+        'trendLines'        : {
+            'line': {
+                'startValue'  : '29.236631705364',
+                'color'       : '9B30FF',
+                'thickness'   : '1',
+                'isTrendZone' : '0',
+                'valueOnRight': '1'
+            }
+        },
+        'styles'            : {
+            'definition' : {'style': {'name': 'MyFirstFontStyle', 'type': 'font', 'isHTML': '1'}},
+            'application': {'apply': {'toObject': 'TOOLTIP', 'styles': 'MyFirstFontStyle'}}
+        }
+    }
+};
